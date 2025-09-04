fe69c8711b2ce4e319730e91587ea2b11534478a
Arailym Serikbay <61643712+arailymmmm@users.noreply.github.com>
2025-09-04 17:06:37 +0500

    Create ddl_gold.sql

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,75 @@
+/*
+===============================================================================
+DDL Script: Create gold Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'gold' schema, dropping existing tables 
+    if they already exist.
+    Run this script to re-define the DDL structure of 'gold' Tables
+===============================================================================
+*/
+
+-- Create schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS silver;
+
+-- Table: crm_cust_info
+DROP TABLE IF EXISTS gold.crm_cust_info;
+CREATE TABLE gold.crm_cust_info (
+    cst_id              INT,
+    cst_key             VARCHAR(50),
+    cst_firstname       VARCHAR(50),
+    cst_lastname        VARCHAR(50),
+    cst_marital_status  VARCHAR(50),
+    cst_gndr            VARCHAR(50),
+    cst_create_date     DATE
+);
+
+-- Table: crm_prd_info
+DROP TABLE IF EXISTS gold.crm_prd_info;
+CREATE TABLE gold.crm_prd_info (
+    prd_id       INT,
+    prd_key      VARCHAR(50),
+    prd_nm       VARCHAR(50),
+    prd_cost     INT,
+    prd_line     VARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+
+-- Table: crm_sales_details
+DROP TABLE IF EXISTS gold.crm_sales_details;
+CREATE TABLE gold.crm_sales_details (
+    sls_ord_num  VARCHAR(50),
+    sls_prd_key  VARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+
+-- Table: erp_loc_a101
+DROP TABLE IF EXISTS gold.erp_loc_a101;
+CREATE TABLE gold.erp_loc_a101 (
+    cid    VARCHAR(50),
+    cntry  VARCHAR(50)
+);
+
+-- Table: erp_cust_az12
+DROP TABLE IF EXISTS gold.erp_cust_az12;
+CREATE TABLE gold.erp_cust_az12 (
+    cid    VARCHAR(50),
+    bdate  DATE,
+    gen    VARCHAR(50)
+);
+
+-- Table: erp_px_cat_g1v2
+DROP TABLE IF EXISTS gold.erp_px_cat_g1v2;
+CREATE TABLE gold.erp_px_cat_g1v2 (
+    id           VARCHAR(50),
+    cat          VARCHAR(50),
+    subcat       VARCHAR(50),
+    maintenance  VARCHAR(50)
+);
